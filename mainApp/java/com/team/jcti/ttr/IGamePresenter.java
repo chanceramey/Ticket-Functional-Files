df0d261502dcedfa8ec7594e936d5cca01d48735
tjense25 <tanner.jensen08@gmail.com>
2018-03-10 13:12:42 -0700

    added IGamePresenter

---
+++ mainApp/java/com/team/jcti/ttr/IGamePresenter.java
@@ -0,0 +1,12 @@
+package com.team.jcti.ttr;
+
+/**
+ * Created by tjense25 on 3/10/18.
+ */
+
+public interface IGamePresenter {
+
+    void displayError(String message);
+    void update();
+    void drawDestCards();
+}
