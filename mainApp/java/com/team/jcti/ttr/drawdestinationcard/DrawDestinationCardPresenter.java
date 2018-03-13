90ad6acb5e8a66c1cb52027952047c43d71ad9a0
Jeff Tingey <jefftingey22@gmail.com>
2018-03-12 22:34:32 -0600

    I think phase 2 is finished. Bless up. Fixed dest card pick bug

--- mainApp/java/com/team/jcti/ttr/drawdestinationcard/DrawDestinationCardPresenter.java
+++ mainApp/java/com/team/jcti/ttr/drawdestinationcard/DrawDestinationCardPresenter.java
@@ -69,7 +69,6 @@ public class DrawDestinationCardPresenter implements Observer, IDrawDestinationC
             rejectedCardPositions[count] = pos+player.getNumDestCards()-3; //checkback
             count++;
         }
-        mClientGameModel.getUserPlayer().removeDestCards(rejectedCardPositions);
 
         if (mClientGameModel.getUserPlayer().isFirstDestPick()) {
             mClientGameModel.turnToast();
